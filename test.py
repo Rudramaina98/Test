8274fa8fd6bb977bd484fde9eda7dba6e185756e
“rudramaina98” <rudramaina.shivani@gmail.com>
2024-12-03 17:37:12 -0500

    adding class &  objects

--- test.py
+++ test.py
@@ -6,3 +6,16 @@ print(a+b)
 first_name= "Shivani"
 last_name= "Rudramaina"
 print(first_name+last_name)
+
+import math
+class Oops:
+    def __init__(self, a):
+        print(a)
+
+    def subract(self, a, b):
+        return math.fabs(a-b)
+
+oops= Oops(3.83)
+val = oops.subract(4, 67)
+print(val)
+
